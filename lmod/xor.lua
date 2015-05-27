37dd74dc91505af9e7df556fbecab24eb3f0f497
TsT <tst2005@gmail.com>
2015-05-27 19:37:27 +0200

    initial running version

---
+++ lmod/xor.lua
@@ -0,0 +1,67 @@
+#!/usr/bin/env lua
+
+-- TODO: add (C) TsT
+
+local bit = require("bit") -- using the luajit
+local bxor = assert(bit.bxor, "bitwise support is required")
+
+local c2b = string.byte
+local string_char = string.char
+local b2c = function(b)
+	return string_char( tonumber( b
+ --bit.band(0xff, b)
+ ) )
+end
+
+local exit = assert(require("os").exit)
+
+local files = {...}
+
+if #files < 2 then
+	print("ERROR: xor.lua <file1> <file2>")
+	exit(1)
+end
+
+local fds = {}
+-- open all
+for i, file in ipairs(files) do
+	local fd
+	if file == "-" then
+		fd = io.stdin
+	else
+		fd = io.open(file, "r")
+	end
+	fds[i]=fd
+end
+
+local eofs = {}
+
+while true do
+
+	local B = nil
+	for i, fd in ipairs(fds) do
+		if not eofs[i] then
+			local c = fd:read(1)
+			if not c then
+				eofs[i]=true
+			else
+				if B == nil then
+					B = c2b(c)
+				else
+					B = bxor(B,c2b(c))
+				end
+			end
+		end
+	end --/for
+	if B~=nil then
+		io.write(b2c(B))
+		B=nil
+	end
+	if #eofs >= #fds then break end
+end --/while
+
+-- close all
+for i, fd in ipairs(fds) do
+	if fd then fd:close() end
+end
+
